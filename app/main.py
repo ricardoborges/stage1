5a54f87142e29229ce06dc184a5cc6854f73252e
Ricardo Borges <ricardoborges@gmail.com>
2018-06-17 11:17:07 -0300

    basic authentication

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request, Response
 from newspaper import Article
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -10,7 +10,27 @@ app.config['JSON_AS_ASCII'] = False
 def hello():
     return "build 3"
 
+def check_auth(username, password):
+    return username == 'admin' and password == '123456'  
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})  
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated    
+
 @app.route("/noticia/<path:url>")
+@requires_auth
 def noticia(url):
     article = Article(url)
     article.download()
