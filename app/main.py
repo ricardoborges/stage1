c53771feb3a76cba0a0d92417be89f608d5e479c
Ricardo Borges <ricardoborges@gmail.com>
2018-06-17 21:37:49 -0300

    api key test

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request, abort
 from newspaper import Article
+from functools import wraps
 
 app = Flask(__name__)
 
 app.config['JSON_AS_ASCII'] = False
 
+def require_appkey(apifunction):
+    @wraps(apifunction)
+    # the new, post-decoration function. Note *args and **kwargs here.
+    def decorated_function(*args, **kwargs):
+        if request.args.get('key') and request.args.get('key') == "698dc19d489c4e4db73e28a713eab07b":
+            return apifunction(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
 @app.route("/")
 def hello():
-    return "build 3"
+    return "build 4"
 
 @app.route("/noticia/<path:url>")
+@require_appkey
 def noticia(url):
     article = Article(url)
     article.download()
